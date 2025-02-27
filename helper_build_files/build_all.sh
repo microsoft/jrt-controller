588cc4b01206285d81068df1ca8133ddadc49af2
Zhihua Lai <zhihualai@microsoft.com>
2025-02-27 19:41:44 +0000

    Remove -j

--- helper_build_files/build_all.sh
+++ helper_build_files/build_all.sh
@@ -34,7 +34,7 @@ if [[ $rebuild == true || ! -d $JRTC_PATH/out ]]; then
     mkdir -p build
     cd build
     cmake ../
-    make -j
+    make
 else
     echo "...............Skipping jrtc build..............."
 fi
