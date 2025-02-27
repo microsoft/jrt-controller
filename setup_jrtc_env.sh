38df0ec4781817f1e819fd92e61e394223c886a1
Doctor Lai <92519564+doctorlai-msrc@users.noreply.github.com>
2025-02-27 14:00:29 +0000

    Minor fixes, code cleanup and Build status badge (#12)
    * Minor fixes
    
    * Cleanup
    
    * Cleanup
    
    * Reverted
    
    ---------
    
    Co-authored-by: Zhihua Lai <zhihualai@microsoft.com>

--- setup_jrtc_env.sh
+++ setup_jrtc_env.sh
@@ -7,5 +7,4 @@ export JRTC_OUT_DIR=$JRTC_PATH/out
 export JRTC_APP_PATH=$JRTC_OUT_DIR/lib/
 export JRTC_PYTHON_INC=$JRTC_PATH/src/wrapper_apis
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JRTC_OUT_DIR/lib
-export PYTHONMALLOC=malloc
 source $JRTC_PATH/jbpf-protobuf/setup_jbpfp_env.sh
