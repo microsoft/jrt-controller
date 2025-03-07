68adcff8275046f44cf3697e6a8b34d42a2f540b
Zhihua Lai <zhihualai@microsoft.com>
2025-03-07 14:11:56 +0000

    Continued work

---
+++ src/controller/jrtc_yaml.c
@@ -0,0 +1,80 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+#include "jrtc_yaml.h"
+
+int parse_yaml_config(const char *filename, config_t *config) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Failed to open YAML file");
+        return -1;
+    }
+
+    yaml_parser_t parser;
+    yaml_event_t event;
+    char key[256] = {0};
+    int in_jrtc_router_config = 0;
+    int in_thread_config = 0;
+    int in_jbpf_io_config = 0;
+
+    if (!yaml_parser_initialize(&parser)) {
+        fprintf(stderr, "Failed to initialize YAML parser\n");
+        fclose(file);
+        return -1;
+    }
+    yaml_parser_set_input_file(&parser, file);
+
+    while (yaml_parser_parse(&parser, &event)) {
+        switch (event.type) {
+            case YAML_SCALAR_EVENT:
+                if (strcmp((char *)event.data.scalar.value, "jrtc_router_config") == 0) {
+                    in_jrtc_router_config = 1;
+                } else if (strcmp((char *)event.data.scalar.value, "thread_config") == 0 && in_jrtc_router_config) {
+                    in_thread_config = 1;
+                } else if (strcmp((char *)event.data.scalar.value, "jbpf_io_config") == 0) {
+                    in_jbpf_io_config = 1;
+                } else if (in_thread_config) {
+                    if (key[0] == '\0') {
+                        strncpy(key, (char *)event.data.scalar.value, sizeof(key) - 1);
+                    } else {
+                        if (strcmp(key, "affinity_mask") == 0) {
+                            config->jrtc_router_config.thread_config.affinity_mask = atoi((char *)event.data.scalar.value);
+                        } else if (strcmp(key, "hash_sched_config") == 0) {
+                            config->jrtc_router_config.thread_config.hash_sched_config = 
+                                (strcmp((char *)event.data.scalar.value, "true") == 0) ? 1 : 0;
+                        }
+                        key[0] = '\0';
+                    }
+                } else if (in_jbpf_io_config) {
+                    if (key[0] == '\0') {
+                        strncpy(key, (char *)event.data.scalar.value, sizeof(key) - 1);
+                    } else {
+                        if (strcmp(key, "jbpf_namespace") == 0) {
+                            strncpy(config->jbpf_io_config.jbpf_namespace, (char *)event.data.scalar.value, 
+                                    sizeof(config->jbpf_io_config.jbpf_namespace) - 1);
+                        } else if (strcmp(key, "jbpf_path") == 0) {
+                            strncpy(config->jbpf_io_config.jbpf_path, (char *)event.data.scalar.value, 
+                                    sizeof(config->jbpf_io_config.jbpf_path) - 1);
+                        }
+                        key[0] = '\0';
+                    }
+                }
+                break;
+            case YAML_MAPPING_END_EVENT:
+                if (in_thread_config) {
+                    in_thread_config = 0;
+                } else if (in_jrtc_router_config) {
+                    in_jrtc_router_config = 0;
+                } else if (in_jbpf_io_config) {
+                    in_jbpf_io_config = 0;
+                }
+                break;
+            default:
+                break;
+        }
+        yaml_event_delete(&event);
+    }
+
+    yaml_parser_delete(&parser);
+    fclose(file);
+    return 0;
+}
--- src/controller/jrtc_yaml.h
+++ src/controller/jrtc_yaml.h
@@ -3,6 +3,30 @@
 #ifndef JRTC_YAML_H
 #define JRTC_YAML_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yaml.h>
 
+typedef struct {
+    int affinity_mask;
+    int hash_sched_config;  // Boolean (0 or 1)
+} thread_config_t;
+
+typedef struct {
+    thread_config_t thread_config;
+} jrtc_router_config_t;
+
+typedef struct {
+    char jbpf_namespace[256];
+    char jbpf_path[256];
+} jbpf_io_config_t;
+
+typedef struct {
+    jrtc_router_config_t jrtc_router_config;
+    jbpf_io_config_t jbpf_io_config;
+} config_t;
+
+int parse_yaml_config(const char *filename, config_t *config);
 
 #endif // JRTC_YAML_H
