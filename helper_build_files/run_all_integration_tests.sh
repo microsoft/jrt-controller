fec9cd4b70c3b455201961b5f9cc393919ae39a3
Zhihua Lai <92519564+doctorlai-msrc@users.noreply.github.com>
2025-03-07 00:25:50 +0000

    Refactor and Speed up the Pipeline (#23)
    * Fix a bug in pipeline to not rebuild jrtc
    
    * Add missing assert.sh
    
    * Fix
    
    * Ignore the out from previous step
    
    * Debug and Fix
    
    * Add more debugging
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

---
+++ helper_build_files/run_all_integration_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+set -ex
+
+if [ -z "$JRTC_PATH" ]; then
+    echo "Error: JRTC_PATH environment variable is not set. Please set it before running this script."
+    exit 1
+fi
+
+pushd $JRTC_PATH/helper_build_files
+
+TEST_CASES=("first_example" "first_example_c" "first_example_py" "advanced_example" "advanced_example_c")
+JRTC_TESTS_OUTPUT=/tmp/jrtc_tests_output.log
+
+for TEST in "${TEST_CASES[@]}"; do
+    echo ".......................................................................................... Running test: $TEST .........................................................................................."
+    rm -rf $JRTC_TESTS_OUTPUT || true
+
+    $JRTC_PATH/helper_build_files/integration_tests.sh $TEST |& tee $JRTC_TESTS_OUTPUT
+        
+    echo ".............................................Output.log: $TEST ............................................."
+    cat $JRTC_TESTS_OUTPUT
+
+    if ! $JRTC_PATH/sample_apps/$TEST/assert.sh $JRTC_TESTS_OUTPUT $TEST; then
+        echo "Test Assertion failed for $TEST"
+        exit 1
+    fi
+        
+    echo ".......................................................................................... Test Passed: $TEST .........................................................................................."
+done
+
+echo "All tests passed!"
