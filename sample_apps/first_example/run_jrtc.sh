1335af1d8056bb887bfc78167e5e6b3360d944b7
Zhihua Lai <zhihualai@microsoft.com>
2025-03-10 14:47:22 +0000

    Re-enable

--- sample_apps/first_example/run_jrtc.sh
+++ sample_apps/first_example/run_jrtc.sh
@@ -6,5 +6,4 @@
 CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 source $CURRENT_DIR/../../setup_jrtc_env.sh
-${JRTC_OUT_DIR}/bin/jrtc 
-# --config=$CURRENT_DIR/test_config.yaml
+${JRTC_OUT_DIR}/bin/jrtc --config=$CURRENT_DIR/test_config.yaml
