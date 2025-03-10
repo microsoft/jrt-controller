22ecbec3dd80be6096e820c39ef0bc5cecb80c47
Zhihua Lai <zhihualai@microsoft.com>
2025-03-10 14:38:56 +0000

    Fix

--- sample_apps/first_example/run_jrtc.sh
+++ sample_apps/first_example/run_jrtc.sh
@@ -6,4 +6,5 @@
 CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 source $CURRENT_DIR/../../setup_jrtc_env.sh
-${JRTC_OUT_DIR}/bin/jrtc --config=$CURRENT_DIR/test_config.yaml
+${JRTC_OUT_DIR}/bin/jrtc 
+# --config=$CURRENT_DIR/test_config.yaml
