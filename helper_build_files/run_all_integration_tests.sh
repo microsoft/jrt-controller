a9d9238a9e492806ea74ac8235bee38f54cb975e
Zhihua Lai <zhihualai@microsoft.com>
2025-03-10 21:01:41 +0000

    Make it a good practice

--- helper_build_files/run_all_integration_tests.sh
+++ helper_build_files/run_all_integration_tests.sh
@@ -18,6 +18,7 @@ JRTC_TESTS_OUTPUT=/tmp/jrtc_tests_output.log
 for TEST in "${TEST_CASES[@]}"; do
     echo ".......................................................................................... Running test: $TEST .........................................................................................."
     rm -rf "$JRTC_TESTS_OUTPUT" || true
+    touch "$JRTC_TESTS_OUTPUT" || { echo "Error: Failed to create $JRTC_TESTS_OUTPUT"; exit 2; }
 
     "$JRTC_PATH/helper_build_files/integration_tests.sh" "$TEST" |& tee "$JRTC_TESTS_OUTPUT"
     if [ "${PIPESTATUS[0]}" -ne 0 ]; then
@@ -26,6 +27,7 @@ for TEST in "${TEST_CASES[@]}"; do
     fi
   
     echo ".............................................Output.log: $TEST ............................................."
+    timeout 10 tail -f "$JRTC_TESTS_OUTPUT" | grep -q .  # Wait until file is non-empty
     cat "$JRTC_TESTS_OUTPUT"
 
     if ! "$JRTC_PATH/sample_apps/$TEST/assert.sh" "$JRTC_TESTS_OUTPUT" "$TEST"; then
