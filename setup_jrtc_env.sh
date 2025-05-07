e55ee13fd9ecc9853e35bca610346b32c612e1d0
Zhihua Lai <zhihualai@microsoft.com>
2025-05-07 09:51:29 +0100

    Check if it is PYTHONMALLOC

--- setup_jrtc_env.sh
+++ setup_jrtc_env.sh
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+export PYTHONMALLOC=malloc
 export JRTC_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 export JRTC_OUT_DIR=$JRTC_PATH/out
 export JRTC_APP_PATH=$JRTC_OUT_DIR/lib/
