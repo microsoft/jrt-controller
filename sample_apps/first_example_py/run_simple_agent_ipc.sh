b3592c4b3284058f77a8cfe1863e2c381e57ba20
Zhihua Lai <zhihualai@microsoft.com>
2025-06-09 14:00:58 +0100

    Revert

--- sample_apps/first_example_py/run_simple_agent_ipc.sh
+++ sample_apps/first_example_py/run_simple_agent_ipc.sh
@@ -7,4 +7,5 @@ CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 source $CURRENT_DIR/../../setup_jrtc_env.sh
 
-LD_PRELOAD=$(gcc -print-file-name=libasan.so) $CURRENT_DIR/simple_agent_ipc/simple_agent_ipc
+# LD_PRELOAD=$(gcc -print-file-name=libasan.so)
+$CURRENT_DIR/simple_agent_ipc/simple_agent_ipc
